d4819b24cad0aac39aad4de4cac3170e8f5d93a8
ming <ming@163.com>
2022-03-28 20:14:12 +0800

    delete unuseful code

--- server/main.py
+++ server/main.py
@@ -1,5 +1,3 @@
-from email import message
-from tkinter.messagebox import NO
 from unittest import result
 from fastapi import FastAPI, Header, File, UploadFile, Form
 from pydantic import BaseModel

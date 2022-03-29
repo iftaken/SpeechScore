ff2d2ea24f9ce9f4d7dba10640b7c2a54e37e9dd
ming <ming@163.com>
2022-03-29 10:20:38 +0800

    fixed build bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,21 @@ import { defineConfig } from "vite"
 import vue from '@vitejs/plugin-vue'
 
 
+
+
 export default defineConfig({
   plugins: [vue()],
+  css: 
+    { preprocessorOptions:
+      { css: 
+        { 
+          charset: false 
+        } 
+      } 
+    },
+  build: {
+      assetsInlineLimit: '2048' // 2kb
+  },
   server: {
     host: "0.0.0.0",
     proxy: {
